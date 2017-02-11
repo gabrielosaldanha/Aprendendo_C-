c067d9dfcf3a4ae6e4194090b3a547843b4f4dac
Gabriel Saldanha <gabrielsaldanha1990@gmail.com>
2017-02-11 17:52:47 -0200

    Create ed01.cs

---
+++ Exercicios_Lista_Sequencial/ed01.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ed01
+{
+    class ed01
+    {
+        static void Main(string[] args)
+        {
+            /* Faça um Programa que mostre a mensagem "Olá mundo!" na tela.*/
+
+            Console.WriteLine("Olá mundo!");
+        }
+    }
+}
