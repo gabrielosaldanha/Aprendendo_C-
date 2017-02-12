2f0362cf42019ab1a76708841a1541afab6f828e
Gabriel Saldanha <gabrielsaldanha1990@gmail.com>
2017-02-12 04:57:19 -0200

    Create ed10.cs

---
+++ Exercicios_Lista_Sequencial/ed10.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ed10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Faça um Programa que peça a temperatura em graus Celsius,
+            transforme e mostre em graus Farenheit.*/
+
+            double tempF, tempC;
+            string temp;
+
+            Console.Write("Informe a temperatura em Celsius: ");
+            temp = Console.ReadLine();
+
+            if (double.TryParse(temp, out tempC))
+            {
+                tempF = (tempC * 1.8 + 32);
+                Console.WriteLine("A temperatura em Farenheit é: {0}", tempF);
+            }
+            else
+            {
+                Console.WriteLine("Você digitou corretamente?");
+            }
+            
+        }
+    }
+}
